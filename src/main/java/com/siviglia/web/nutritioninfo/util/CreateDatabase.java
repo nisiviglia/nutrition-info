ee0e4b737c1b16e71cd123aad83dbffd2e8f8eea
nisiviglia <ns533@njit.edu>
2019-06-30 23:36:43 -0400

    Added exec point for database tool. Added empty crate database file. to run: mvn exec:java@create-database

---
+++ src/main/java/com/siviglia/web/nutritioninfo/util/CreateDatabase.java
@@ -0,0 +1,22 @@
+/** 
+ * Copyright (C) 2019 Nicholas Siviglia - All Rights Reserved. 
+ * Unauthorized copying of this software, via any medium is strictly prohibited. 
+ * Proprietary and confidential.
+**/
+/**
+ * @file CreateDatabaseFromFolder.java
+ * @brief controller that handles all search functions.
+ * @author Nicholas Siviglia
+ * @version 1.0
+ * @date 2019-06-30
+ */
+
+package com.siviglia.web.nutritioninfo.util;
+
+public class CreateDatabase {
+
+    public static void main( String[] args ){
+        System.out.println("good");    
+    }
+}
+
