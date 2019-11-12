55ef2e304b056969c9e2b874193182cf59d6647b
nisiviglia <ns533@njit.edu>
2019-11-11 19:57:05 -0500

    Added all nutrients with db codes.

--- src/main/app/src/product/DailyValues.js
+++ src/main/app/src/product/DailyValues.js
@@ -8,82 +8,103 @@ module.exports = {
 
     nutrients:  [
         {
+            dbCode: 1008,
             dbName: "Energy",
             displayName: "Calories",
             dailyValue: 2000,
             bold: true
         },   
         {
+            dbCode: 1004,
             dbName: "Total lipid (fat)",
             displayName: "Total Fat",
             dailyValue: 65,
             bold: true
         },
         {
+            dbCode: 1258,
             dbName: "Fatty acids, total saturated",
             displayName: "Saturated Fat",
             dailyValue: 20
         },
         {
+            dbCode: 1257,
             dbName: "Fatty acids, total trans",
             displayName: "Trans Fat",
             dailyValue: -1
         },
         {
+            dbCode: 1293,
             dbName: "Fatty acids, total polyunsaturated",
             displayName: "Polyunsaturated Fat",
             dailyValue: -1
         },
         {
+            dbCode: 1292,
             dbName: "Fatty acids, total monounsaturated",
             displayName: "Monounsaturated Fat",
             dailyValue: -1
         },
         {
+            dbCode: 1253,
             dbName: "Cholesterol",
             displayName: "Cholesterol",
             dailyValue: 300,
             bold: true
         },
         {
+            dbCode: 1093,
             dbName: "Sodium, Na",
             displayName: "Sodium",
             dailyValue: 2400,
             bold: true
         },
         {   
+            dbCode: 1092,
             dbName: "Potassium, K",
             displayName: "Potassium",
             dailyValue: 3500,
             bold: true
         },
         {
+            dbCode: 1099,
+            dbName: "Fluoride, F",
+            displayName: "Fluoride",
+            dailyValue: 4,
+        },
+        {
+            dbCode: 1005,
             dbName: "Carbohydrate, by difference",
             displayName: "Total Carbohydrate",
             dailyValue: 300,
             bold: true
         },
         {
+            dbCode: 1079,
             dbName: "Fiber, total dietary",
             displayName: "Dietary Fiber",
             dailyValue: 25
         },
         {
+            dbCode: 1082,
             dbName: "Fiber, soluble",
             displayName: "Soluble Fiber",
             dailyValue: -1
         },
         {
+            dbCode: 1084,
             dbName: "Fiber, insoluble",
             displayName: "Insoluble Fiber",
             dailyValue: -1
         },
         {
+            dbCode: 2000,
             dbName: "Sugars, total including NLEA",
             displayName: "Sugars",
             dailyValue: -1 
         },
         {
+            dbCode: 1003,
             dbName: "Protein",
             displayName: "Protein",
             dailyValue: 50,
@@ -93,149 +114,502 @@ module.exports = {
 
     vitamins: [
         {
+            dbCode: 1104,
             dbName: "Vitamin A, IU",
             displayName: "Vitamin A",
             dailyValue: 5000,
         },
         {
+            dbCode: 1162,
             dbName: "Vitamin C, total ascorbic acid",
             displayName: "Vitamin C",
             dailyValue: 60,
         },
         {
+            dbCode: 1087,
             dbName: "Calcium, Ca",
             displayName: "Calcium",
             dailyValue: 1000,
         },
         {
+            dbCode: 1089,
             dbName: "Iron, Fe",
             displayName: "Iron",
             dailyValue: 18,
         },
         {
+            dbCode: 1110,
             dbName: "Vitamin D",
             displayName: "Vitamin D",
             dailyValue: 400,
         },
         {
+            dbCode: 1114,
             dbName: "Vitamin D (D2 + D3)",
             displayName: "Vitamin D (D2 + D3)",
             dailyValue: 400,
         },
         {
+            dbCode: 1112,
             dbName: "Vitamin D3 (cholecalciferol)",
             displayName: "Vitamin D3",
             dailyValue: 400,
         },
         {
+            dbCode: 1158,
             dbName: "Vitamin E",
             displayName: "Vitamin E",
             dailyValue: 30,
         },
         {
+            dbCode: 1109,
             dbName: "Vitamin E (alpha-tocopherol)",
             displayName: "Vitamin E",
             dailyValue: 30,
         },
         {
+            dbCode: 1124,
             dbName: "Vitamin E (label entry primarily)",
             displayName: "Vitamin E",
             dailyValue: 30,
         },
         {
+            dbCode: 1185,
             dbName: "Vitamin K (phylloquinone)",
             displayName: "Vitamin K",
             dailyValue: 80,
         },
         {
+            dbCode: 1165,
             dbName: "Thiamin",
             displayName: "Thiamin",
             dailyValue: 1.5,
         },
         {
+            dbCode: 1166,
             dbName: "Riboflavin",
             displayName: "Riboflavin",
             dailyValue: 1.7,
         },
         {
+            dbCode: 1167,
             dbName: "Niacin",
             displayName: "Niacin",
             dailyValue: 20,
         },
         {
+            dbCode: 1175,
             dbName: "Vitamin B-6",
             displayName: "Vitamin B6",
             dailyValue: 2.0,
         },
         {
+            dbCode: 1177,
             dbName: "Folate, total",
             displayName: "Folate",
             dailyValue: 400,
         },
         {
+            dbCode: 1178,
             dbName: "Vitamin B-12",
             displayName: "Vitamin B12",
             dailyValue: 6.0,
         },
         {
+            dbCode: 1176,
             dbName: "Biotin",
             displayName: "Biotin",
             dailyValue: 300,
         },
         {
+            dbCode: 1170,
             dbName: "Pantothenic acid",
             displayName: "Pantothenic acid",
             dailyValue: 10,
         },
         {
+            dbCode: 1091,
             dbName: "Phosphorus, P",
             displayName: "Phosphorus",
             dailyValue: 1000,
         },
         {
+            dbCode: 1100,
             dbName: "Iodine, I",
             displayName: "Iodine",
             dailyValue: 150,
         },
         {
+            dbCode: 1090,
             dbName: "Magnesium, Mg",
             displayName: "Magnesium",
             dailyValue: 400,
         },
         {
+            dbCode: 1095,
             dbName: "Zinc, Zn",
             displayName: "Zinc",
             dailyValue: 15,
         },
         {
+            dbCode: 1103,
             dbName: "Selenium, Se",
             displayName: "Selenium",
             dailyValue: 70,
         },
         {
+            dbCode: 1098,
             dbName: "Copper, Cu",
             displayName: "Copper",
             dailyValue: 2.0,
         },
         {
+            dbCode: 1101,
             dbName: "Manganese, Mn",
             displayName: "Manganese",
             dailyValue: 2.0,
         },
         {
+            dbCode: 1096,
             dbName: "Chromium, Cr",
             displayName: "Chromium",
             dailyValue: 120,
         },
         {
+            dbCode: 1102,
             dbName: "Molybdenum, Mb",
             displayName: "Molybdenum",
             dailyValue: 75,
         },
         {
+            dbCode: 1088,
             dbName: "Chlorine, Cl",
             displayName: "Chloride",
             dailyValue: 3400,
+        },
+        {
+            dbCode: 1007,
+            dbName: "Ash",
+            displayName: "Ash",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1009,
+            dbName: "Starch",
+            displayName: "Starch",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1011,
+            dbName: "Glucose (dextrose)",
+            displayName: "Glucose",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1012,
+            dbName: "Fructose",
+            displayName: "fructose",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1013,
+            dbName: "Lactose",
+            displayName: "Lactose",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1018,
+            dbName: "Alcohol, ethyl",
+            displayName: "Alcohol, ethyl",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1026,
+            dbName: "Acetic acid",
+            displayName: "Acetic acid",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1038,
+            dbName: "Lactic acid",
+            displayName: "Lactic acid",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1051,
+            dbName: "Water",
+            displayName: "Water",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1056,
+            dbName: "Sorbitol",
+            displayName: "Sorbitol",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1057,
+            dbName: "Caffeine",
+            displayName: "Caffeine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1057,
+            dbName: "Caffeine",
+            displayName: "Caffeine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1072,
+            dbName: "Carbohydrate, other",
+            displayName: "Carbohydrate, other",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1078,
+            dbName: "Xylitol",
+            displayName: "Xylitol",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1081,
+            dbName: "Ribose",
+            displayName: "Ribose",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1086,
+            dbName: "Total sugar alcohols",
+            displayName: "Total sugar alcohols",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1107,
+            dbName: "Carotene, beta",
+            displayName: "Carotene, beta",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1123,
+            dbName: "Lutein + zeaxanthin",
+            displayName: "Lutein + zeaxanthin",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1180,
+            dbName: "Choline, total",
+            displayName: "Choline",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1181,
+            dbName: "Inositol",
+            displayName: "Inositol",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1186,
+            dbName: "Folic acid",
+            displayName: "Folic acid",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1190,
+            dbName: "Folate, DFE",
+            displayName: "Folate, DFE",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1196,
+            dbName: "Choline, from phosphotidyl choline",
+            displayName: "Choline, from phosphotidyl choline",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1210,
+            dbName: "Tryptophan",
+            displayName: "Tryptophan",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1211,
+            dbName: "Threonine",
+            displayName: "Threonine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1212,
+            dbName: "Isoleucine",
+            displayName: "Isoleucine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1213,
+            dbName: "Leucine",
+            displayName: "Leucine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1214,
+            dbName: "Lysine",
+            displayName: "Lysine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1215,
+            dbName: "Methionine",
+            displayName: "Methionine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1215,
+            dbName: "Methionine",
+            displayName: "Methionine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1216,
+            dbName: "Cystine",
+            displayName: "Cystine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1217,
+            dbName: "Phenylalanine",
+            displayName: "Phenylalanine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1218,
+            dbName: "Tyrosine",
+            displayName: "Tyrosine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1219,
+            dbName: "Valine",
+            displayName: "Valine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1220,
+            dbName: "Arginine",
+            displayName: "Arginine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1221,
+            dbName: "Histidine",
+            displayName: "Histidine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1222,
+            dbName: "Alanine",
+            displayName: "Alanine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1223,
+            dbName: "Aspartic acid",
+            displayName: "Aspartic acid",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1224,
+            dbName: "Glutamic acid",
+            displayName: "Glutamic acid",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1225,
+            dbName: "Glycine",
+            displayName: "Glycine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1226,
+            dbName: "Proline",
+            displayName: "Proline",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1227,
+            dbName: "Serine",
+            displayName: "Serine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1232,
+            dbName: "Cysteine",
+            displayName: "Cysteine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1233,
+            dbName: "Glutamine",
+            displayName: "Glutamine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1234,
+            dbName: "Taurine",
+            displayName: "Taurine",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1235,
+            dbName: "Sugars, added",
+            displayName: "Sugars, added",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1261,
+            dbName: "8:0",
+            displayName: "8:0",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1262,
+            dbName: "10:0",
+            displayName: "10:0",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1263,
+            dbName: "12:0",
+            displayName: "12:0",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1269,
+            dbName: "18:2",
+            displayName: "18:2",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1316,
+            dbName: "18:2 n-6 c,c",
+            displayName: "18:2 n-6 c,c",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1368,
+            dbName: "Epigallocatechin-3-gallate",
+            displayName: "Epigallocatechin-3-gallate",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1403,
+            dbName: "Inulin",
+            displayName: "Inulin",
+            dailyValue: -1,
+        },
+        {
+            dbCode: 1404,
+            dbName: "18:3 n-3 c,c,c (ALA)",
+            displayName: "18:3 n-3 c,c,c (ALA)",
+            dailyValue: -1,
         },
     ]
 }
